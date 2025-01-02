abe8142f2a8dcd248d1e03d53c6b0b4b2a1b3d64
judoree <135936771+judoree@users.noreply.github.com>
2025-01-02 15:57:43 +0900

    WikipediaRetriever

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,4 +1,49 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+st.set_page_config(
+    page_title=" QuizGPT",
+    page_icon="❓",
+)
+
+
+@st.cache_data(show_spinner="Loading file....")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
 
 
 st.title("QuizGPT GPT")
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt of .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriver = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia..."):
+                docs = retriver.get_relevant_documents(topic)
