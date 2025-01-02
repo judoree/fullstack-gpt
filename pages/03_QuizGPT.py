9449f4abda561a4bacb8dcc687165adc055645d3
judoree <135936771+judoree@users.noreply.github.com>
2025-01-02 16:52:31 +0900

    Question Prompt

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from langchain.retrievers import WikipediaRetriever
 from langchain.chat_models import ChatOpenAI
+from langchain.retrievers import WikipediaRetriever
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
-
 
 st.set_page_config(
     page_title=" QuizGPT",
@@ -27,11 +28,17 @@ def split_file(file):
     return docs
 
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
 st.title("QuizGPT GPT")
 
 llm = ChatOpenAI(
     temperature=0.1,
-    model="gpt-4o-mini ",
+    model="gpt-4o-mini",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
 )
 
 
@@ -69,4 +76,44 @@ if not docs:
     """
     )
 else:
-    st.write(docs)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+            )
+        ]
+    )
+
+    chain = {"context": format_docs} | prompt | llm
+
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
