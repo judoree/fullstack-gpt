00141dfcf2a067485ae2d9c704341bdede2b2dc3
judoree <135936771+judoree@users.noreply.github.com>
2025-01-03 19:11:49 +0900

    Parsing Function

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,12 +1,33 @@
 import streamlit as st
-from langchain.document_loaders import SitemapLoader
+from langchain.document_loaders import SitemapLoader, text
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return str(soup.get_text()).replace("\n", "")
 
 
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
-    loader = SitemapLoader(url)
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        filter_urls=[
+            r"^(.*\/the-podcast\/).*",
+        ],
+        parsing_function=parse_page,
+    )
     loader.requests_per_second = 1
-    docs = loader.load()
+    docs = loader.load_and_split(text_splitter=splitter)
     return docs
 
 
